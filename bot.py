77daadb4b1e8785a1a65cce5d885cf06890934bb
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-12-14 16:54:35 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,20 @@
 import telegram.ext
 import random
+import requests
+import json
+def meaning(word):
+    url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word.lower()
+
+    r = requests.get(url)
+    r = r.json()
+
+    data = r[0]["meanings"][0]["definitions"]
+    answer = data[0]["definition"]
+    try:
+        answer = f"{answer} Also can be described as {data[1]['definition']}"
+    except:
+        pass
+    return(answer)
 
 greet = ["👋", "Hi 👋", "Hello 👋", "Hey 👋"]
 
@@ -35,19 +50,27 @@ def contact(update, context):
 def handle_message(update, context):
      a = 0
      text = update.message.text
-     if 'hi' in text or 'hello' in text or 'hey' in text or 'Hello' in text or 'Hello' in text or 'Hey' in text:
+     print(text)
+     if 'hi' in text or 'hello' in text or 'hey' in text:
           a+=1
           update.message.reply_text(random.choice(greet))
-     if 'my name' in text or "MY Name" in text or "My name" in text or "myname" in text:
+     if 'my name' in text or "myname" in text:
           a+=1
           user = update.message.from_user
           update.message.reply_text(f"Your Name is {user.first_name}")
-     if 'my username' in text or "MY Username" in text or "My username" in text or "myusername" in text:
+     if 'my username' in text or "myusername" in text:
           a+=1
           user = update.message.from_user
           update.message.reply_text(f"Your Username is {user.username}")
+     if "what is" in text and ("meaning" in text or "definition" in text) or "what's" in text and ("meaning" in text or "definition" in text) or "define" in text:
+         a+=1
+         text = text.split(" ")
+         text = text[-1]
+         update.message.reply_text(meaning(text))
      if a < 1:
           update.message.reply_text(f"You Said: {text}. I cant Reply You at the moment.")
+
+
 
 
 updater = telegram.ext.Updater(TOKEN, use_context=True)
